5bac3c884b88a484076a442240e1bbc71cfa9909
Gui V. Ambrogi <guiamb.2022@alunos.utfpr.edu.br>
2022-10-21 22:27:30 -0300

    Nariz

--- main.cpp
+++ main.cpp
@@ -2,13 +2,25 @@
 // Created by doglasrocha on 10/6/22.
 //
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
 int main(void)
 {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    /*sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);*/
 
+    sf::Texture textura;
+    if (!textura.loadFromFile("texturas/narigudo.png")) 
+    {
+        std::cout << "Não possivel carregar a textura." << std::endl;
+        return 0;
+    }
+
+    sf::Sprite narigudo;
+    narigudo.setTexture(textura);
+    
     while (window.isOpen())
     {
         sf::Event event;
@@ -19,25 +31,25 @@ int main(void)
         }
 
         sf::FloatRect BorderColl(0, 0, 1000, 1000);
-        sf::Vector2f BlobPosition = shape.getPosition();
+        sf::Vector2f BlobPosition = narigudo.getPosition();
         bool CheckBorderColl = BorderColl.contains(BlobPosition);
         if (CheckBorderColl)
         {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-                shape.move(1, 0);
+                narigudo.move(1, 0);
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-                shape.move(-1, 0);
+                narigudo.move(-1, 0);
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-                shape.move(0, -1);
+                narigudo.move(0, -1);
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-                shape.move(0, 1);
+                narigudo.move(0, 1);
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(narigudo);
         // shape.move(1, 1);
         sf::sleep(sf::milliseconds(5));
         window.display();
